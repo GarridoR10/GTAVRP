b189b00b73597498185ee3f6ac76c96e1a3d5c9e
Firsty <bazerave@online.no>
2019-05-25 02:50:25 +0200

    Edited holdups

--- resources/[esx]/esx_holdup/server/esx_holdup_sv.lua
+++ resources/[esx]/esx_holdup/server/esx_holdup_sv.lua
@@ -56,7 +56,7 @@ AddEventHandler('esx_holdup:rob', function(robb)
 
 		if rob == false then
 
-			if(cops > 2)then
+			if(cops < 0)then
 
 				rob = true
 				for i=1, #xPlayers, 1 do
--- resources/[esx]/esx_holdupbank/config.lua
+++ resources/[esx]/esx_holdupbank/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 Config.Locale = 'en'
-Config.NumberOfCopsRequired = 2
+Config.NumberOfCopsRequired = 0
 
 Banks = {
 	["fleeca"] = {
